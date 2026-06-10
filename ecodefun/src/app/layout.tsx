b83549ef0daa232de94f7f86140b12fee9d90062
AbindentXtreme <smsinchanmaitra2007@gmail.com>
2026-06-10 16:12:15 +0000

    Added adsense

--- ecodefun/src/app/layout.tsx
+++ ecodefun/src/app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5359516994667565"
+          crossOrigin="anonymous"></script>
+      </head>
       <body>{children}</body>
     </html>
   );
